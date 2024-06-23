1f851415953c78a63bf8cf85063d59e8851cde5f
solver4444 <114271535+solver4444@users.noreply.github.com>
2024-06-23 11:50:40 -0700

    Create navbar.js
    script to fill navbar

---
+++ navbar.js
@@ -0,0 +1,7 @@
+// Script to fill all navbar classes in other pages with navbar from navbar.html
+const nav = document.querySelector('.navbar')
+fetch(/navbar.html)
+      .then(res=>res.text())
+      .then(data=>{
+        nav.innerHTML=data
+      })
