0dc4e82a7735db248f46700f4b57528e012b9226
solver4444 <114271535+solver4444@users.noreply.github.com>
2024-06-23 11:54:40 -0700

    forgot quotation marks :)

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 // Script to fill all navbar classes in other pages with navbar from navbar.html
 const nav = document.querySelector('.navbar')
-fetch(/navbar.html)
+fetch('/navbar.html')
       .then(res=>res.text())
       .then(data=>{
         nav.innerHTML=data
